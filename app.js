1feb55f98f57458d064002992f5b5fcb08b2ac9f
Nathan <ndawit611@gmail.com>
2024-02-17 13:05:46 +0300

    Broke down API calls into clean functions

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const form = document.querySelector(".form");
 
-const getTemp = async() => {
+async function getCity(city) {
+    let latitude, longitude;
+    const res = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
     try {
-        const res = await 
-        console.log(res.data);
-    } catch(e) {
-        console.log("Error!", e);
+        latitude = res.data.results[0].latitude;
+        longitude = res.data.results[0].longitude;
+    } catch (e) {
+        console.log("Error", e);
     }
+    return [latitude, longitude];
 }
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const city = form.elements.query.value;
-    let latitude, longitude;
-    axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`)
+async function getWeather(city) {
+    cityLatitude = await getCity(city).then(res => res[0]);
+    cityLongitude = await getCity(city).then(res => res[1]);
+
+    axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${cityLatitude}&longitude=${cityLongitude}&hourly=temperature_2m,precipitation_probability&timezone=auto&forecast_days=1`)
     .then(res => {
         try {
-            latitude = res.data.results[0].latitude;
-            longitude = res.data.results[0].longitude;
-
-            axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation_probability&timezone=auto&forecast_days=1`)
-            .then(res => {
-                try {
-                    console.log(res.data);
-                } catch (e) {
-                    console.log("Error!", e);
-                }
-            })
+            console.log(res.data);
         } catch (e) {
             console.log("Error!", e);
         }
     })
+}
+
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const city = form.elements.query.value;
+    getWeather(city);  
 })
