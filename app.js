22297bd718cc730de68a9b41dc14b3a0c5195a03
Nathan <ndawit611@gmail.com>
2024-02-28 17:18:14 +0300

    Added max/min temperatures for next 6 days

--- app.js
+++ app.js
@@ -6,6 +6,47 @@ const useLocation = document.querySelector(".location-btn")
 const loader = document.querySelector(".loader");
 const contentCont = document.querySelector(".content-container");
 
+function apiFunctionWrapper() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(successResponse => {
+            resolve(successResponse);
+        }, (errorResponse) => {
+            reject(errorResponse);
+        });
+    });
+}
+
+function getFormattedTime(time) {
+    const timeString12hr = new Date(time)
+        .toLocaleTimeString('en-US',
+            { hour12: true, hour: 'numeric', minute: 'numeric' }
+        );
+    return `Time: ${timeString12hr}`;
+}
+
+function getToday(res) {
+    let hour = document.createElement("div");
+    hour.innerHTML = `${getFormattedTime(res.data.current.time)} ${res.data.timezone_abbreviation}`;
+
+    let temp = document.createElement("div");
+    temp.innerHTML = `Temperature: ${res.data.current.temperature_2m}${res.data.current_units.temperature_2m}`;
+
+    let today = document.createElement("div");
+    today.append(hour, temp);
+    today.classList.add("today");
+
+    cont.append(today);
+}
+
+function getForecast(res) {
+    for (let i = 1; i < res.data.daily.temperature_2m_max.length; i++) {
+        let forecast = document.createElement("div");
+        forecast.innerHTML = `Max: ${res.data.daily.temperature_2m_max[i]}, Min: ${res.data.daily.temperature_2m_min[i]}`;
+        forecast.classList.add("forecast")
+        cont.append(forecast);
+    }
+}
+
 async function getCity(city) {
     let latitude, longitude;
 
@@ -18,16 +59,6 @@ async function getCity(city) {
     }
 
     return [latitude, longitude];
-}
-
-function apiFunctionWrapper() {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(successResponse => {
-            resolve(successResponse);
-        }, (errorResponse) => {
-            reject(errorResponse);
-        });
-    });
 }
 
 async function getCoords() {
@@ -53,24 +84,15 @@ async function getWeather(city) {
         cityLongitude = await getCoords().then(res => res[1]);
     }
 
-    axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${cityLatitude}&longitude=${cityLongitude}&current=temperature_2m,relative_humidity_2m,is_day&timezone=auto&forecast_days=1`)
+    axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${cityLatitude}&longitude=${cityLongitude}&current=temperature_2m,relative_humidity_2m,is_day,precipitation&daily=temperature_2m_max,temperature_2m_min&timezone=auto&forecast_days=7`)
     .then(res => {
         try {
             console.log(res.data);
-            let hour = document.createElement("div");
-            const timeString12hr = new Date(res.data.current.time)
-                .toLocaleTimeString('en-US',
-                    { hour12: true, hour: 'numeric', minute: 'numeric' }
-                );
-            hour.innerHTML = `Time: ${timeString12hr} ${res.data.timezone_abbreviation}`;
 
-            let temp = document.createElement("div");
-            temp.innerHTML = `Temperature: ${res.data.current.temperature_2m}${res.data.current_units.temperature_2m}`;
+            getToday(res);
 
-            let today = document.createElement("div");
-            today.append(hour, temp);
-            today.classList.add("today")
-            cont.append(today);
+            getForecast(res);
+            
         } catch (e) {
             console.log("Error!", e);
         }
@@ -88,7 +110,7 @@ searchForm.addEventListener("submit", async e => {
     const city = searchForm.elements.query.value;
     
     loader.style.display = "block";
-    await getWeather(city);  
+    await getWeather(city); 
     loader.style.display = "none";
 
     contentCont.children[0].children[0].innerHTML = city;
